e142c457ad10d645271f7c618dcb5f8aea543658
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2024-04-08 00:52:18 +0530

    create text chunk creation functionality

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+
 from PyPDF2 import PdfReader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 def get_pdf_text(pdfs):
@@ -9,6 +11,12 @@ def get_pdf_text(pdfs):
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
+
+
+def create_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, length_function = len)
+    chunks = text_splitter.split_text(text)
+    return chunks
 
 
 def main():
@@ -27,7 +35,10 @@ def main():
             with st.spinner("Processing"):
                 # fetch file text
                 raw_text = get_pdf_text(files)
-                # get the text chunks
+
+                # create text chunks
+                chunks = create_text_chunks(raw_text)
+
                 # create vector store
 
 if __name__ == "__main__":
