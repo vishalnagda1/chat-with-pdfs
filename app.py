b49f2d6ab83236ed74b9e408453689fd4d0a9f7d
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2024-04-07 23:56:53 +0530

    create streamlit gui

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+def main():
+    title = "Chat with PDFs"
+    icon = ":books:"
+    st.set_page_config(page_title=title, page_icon=icon)
+
+    st.header(f"{title} {icon}")
+    st.text_input("Ask a question about your PDFs:")
+
+    with st.sidebar:
+        st.subheader("Your PDFs")
+        st.file_uploader("Upload your PDFs here and click on 'Process'")
+        st.button("Process")
+
+if __name__ == "__main__":
+    main()
