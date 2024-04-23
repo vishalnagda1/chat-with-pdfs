f51adb3e897f5ae363a58c475be6b7a963c3d092
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2024-04-23 22:15:44 +0530

    create a chroma db client

---
+++ chroma.py
@@ -0,0 +1,41 @@
+# Reference Document: https://python.langchain.com/docs/integrations/vectorstores/chroma/
+
+import traceback
+
+import chromadb
+from chromadb.config import Settings
+from langchain_chroma import Chroma
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from constants import OLLAMA_BASE_URL, CHROMA_DB_LOCAL_PATH, CHROMA_SERVER_HOST, CHROMA_SERVER_HTTP_PORT, CHROMA_USE_LOCAL
+
+_OEMBED = OllamaEmbeddings(base_url=OLLAMA_BASE_URL, model="nomic-embed-text")
+
+CLIENT_SETTINGS = {
+    "allow_reset": False,
+    "anonymized_telemetry": False,
+    "is_persistent": True,
+}
+
+
+def connect_chroma():
+    """
+    Connects to the Chroma DB.
+    Returns:
+        chromadb.PersistentClient or chromadb.HttpClient
+    """
+    _chroma = None
+    if CHROMA_USE_LOCAL:
+        _chroma = chromadb.PersistentClient(
+            path=CHROMA_DB_LOCAL_PATH,
+            settings=Settings(**CLIENT_SETTINGS),
+        )
+    else:
+        _chroma = chromadb.HttpClient(
+            host=CHROMA_SERVER_HOST,
+            port=CHROMA_SERVER_HTTP_PORT,
+            settings=Settings(**CLIENT_SETTINGS),
+        )
+    return _chroma
+
